d2a12db72570cc903a80f78a80b42c93083a84ef
H (إتش ) <heshamscorpe55@gmail.com>
2018-06-21 17:34:27 +0200

    Initial mason file

---
+++ src/mason.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+const mason = require('commander');
+const { version } = require('./package.json');
+
+// commands
+const create = require('./commands/create');
+const setup = require('./commands/setup');
+
+mason
+    .version(version);
+
+mason
+    .command('setup [env]')
+    .description('run setup commands for all envs')
+    .action(setup);
+
+mason
+    .command('create <ticketId>')
+    .description('creates a new game')
+    .action(create);
+
+mason
+    .command('*')
+    .action(() => {
+        mason.help();
+    });
+
+mason.parse(process.argv);
+
+if (!mason.args.length) {
+    mason.help();
+}
